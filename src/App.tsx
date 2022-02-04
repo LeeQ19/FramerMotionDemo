0d6c097583678582a4059c58495fbd89d693753f
LeeQ19 <leekw1994@gmail.com>
2022-02-05 05:21:27 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AnimatePresence } from "framer-motion";
+import { useState } from "react";
+import { Wrapper, Title, Grid, Box, Overlay, Circle, Button } from "./components/customElements";
 
 function App() {
+  const [selectedId, setSelectedId] = useState<null | string>(null);
+  const [circleId, setCircleId] = useState(0);
+  const numbers = [...Array(4)].map((v, i) => i.toString());
+  console.log(numbers);
+
+  const box = {
+    whileHover0: { scale: 1.1, originX: 1, originY: 1, transition: { bounce: 0 }},
+    whileHover1: { scale: 1.1, originX: 0, originY: 1, transition: { bounce: 0 }},
+    whileHover2: { scale: 1.1, originX: 1, originY: 0, transition: { bounce: 0 }},
+    whileHover3: { scale: 1.1, originX: 0, originY: 0, transition: { bounce: 0 }},
+  };
+
+  const overlay = {
+    hidden: { backgroundColor: "rgba(0, 0, 0, 0)" },
+    visible: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
+  };
+
+  const button = {
+    whileHover: { scale: 1.1, border: "1.5px solid #eb4d4b" },
+    whileTap: { color: "#eb4d4b"},
+  };
+
+  const handleButton = () => {
+    setCircleId((v) => (v + 1) % 4);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>Framer Motion Demo</Title>
+      <Grid>
+        {numbers.map((n) => (
+          <Box
+            variants={box}
+            whileHover={`whileHover${n}`}
+            onClick={() => setSelectedId(n)}
+            key={n}
+            layoutId={n}
+          >
+            {circleId.toString() === n && <Circle layoutId="c" />}
+          </Box>
+        ))}
+      </Grid>
+      <AnimatePresence>
+        {selectedId && (
+          <Overlay
+            variants={overlay}
+            onClick={() => setSelectedId(null)}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+          >
+            <Box layoutId={selectedId} style={{ width: "60%", height: "90%" }}>
+              {circleId.toString() === selectedId &&
+                <Circle style={{ height: "15vmin", width: "15vmin" }} layoutId="c" />
+              }
+            </Box>
+          </Overlay>
+        )}
+      </AnimatePresence>
+      <Button
+        variants={button}
+        whileHover="whileHover"
+        whileTap="whileTap"
+        onClick={handleButton}
+      >
+        Switch
+      </Button>
+    </Wrapper>
   );
 }
 
---
+++ src/components/customElements.tsx
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+export const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  font-size: 3.5rem;
+  font-weight: 700;
+`;
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  width: 50%;
+  height: 75%;
+  gap: 10px;
+`;
+
+export const Box = styled(motion.div)`
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 2%;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const Overlay = styled(motion.div)`
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const Circle = styled(motion.div)`
+  height: 7vmin;
+  width: 7vmin;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 50%;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  cursor: pointer;
+`;
+
+export const Button = styled(motion.button)`
+  height: 40px;
+  width: 100px;
+  font-size: 1.1rem;
+  font-weight: 700;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  border: none;
+`;
